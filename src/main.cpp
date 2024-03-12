9fb142dd0c11d806d5b3530d0dd31ef378a20bd6
Sayemahamed <sayemahamed183@gmail.com>
2024-03-12 21:00:29 +0600

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,107 +1,45 @@
 #include <Arduino.h>
-bool rightF = false, rightR = false, pRightF = false, pRightR = false;
-bool leftF = false, leftR = false, pLeftF = false, pLeftR = false;
-bool lineFollowBot = false;
-int commandBuffer[10];
-int getCommand(){
-  int temp = commandBuffer[0];
-  for(int i=0;i<9;i++){
-    commandBuffer[i]=commandBuffer[i+1];
-  }
-  commandBuffer[9]=0;
-  return temp;
-}
-void addCommand(int cmd){
-  for(int i=0;i<9;i++){
-    commandBuffer[i]=commandBuffer[i+1];
-  }
-  commandBuffer[9]=cmd;
-}
-void reset(int timer){
-    digitalWrite(8,0);
-    digitalWrite(9,0);
-    digitalWrite(10,0);
-    digitalWrite(11,0);
-    pRightF= rightF;
-    rightF=false;
-    pRightR =rightR;
-    rightR=false;
-    pLeftF =leftF;
-    leftF= false;
-    pLeftR= leftR;
-    leftR= false;
-    delay(timer);
-}
-void executeRemoteCommand(){
-  if(leftF and !leftR){
-    digitalWrite(11,1);
-    digitalWrite(10,0);
-  }
-  if(leftR and !leftF){
-
-    digitalWrite(10,1);
-    digitalWrite(11,0);
-  }
-  if(rightF and !rightR){
-    digitalWrite(8,1);
-    digitalWrite(9,0);
-  }
-  if(rightR and !rightF){
-    digitalWrite(9,1);
-    digitalWrite(8,0);
-  }
-  delay(200);
-}
-void command()
-{
-  Serial.println("hello");
-  int temp=Serial.read();
-  Serial.println(temp);
-  if (temp == 49)
-  {
-    leftF = true;
-    leftR = false;
-  }
-  if(temp==51){
-    leftR=true;
-    leftF=false;
-  }
-  if(temp==50){
-    rightF=true;
-    rightR=false;
-  }
-  if(temp==52){
-    rightR=true;
-    rightF=false;
-  }
-}
-void setMode()
-{
-  if (Serial.available() > 0)
-  {
-    if (Serial.read() == 53)
-      lineFollowBot = true;
-  }
-  if (Serial.available() > 0)
-  {
-    if (Serial.read() == 54)
-      lineFollowBot = false;
-  }
-}
+bool right = false;
+bool left = false;
+int rightCount=0,leftCount=0;
 void setup()
 {
-  Serial.begin(9600);
 }
 void loop()
 {
-  setMode();
-  if (lineFollowBot)
-  {
+  bool a = digitalRead(2);
+  bool b = digitalRead(3);
+  if (!a){
+    right = true;
+    rightCount=0;
   }
-  else
-  {
-    getCommand();
-    // executeRemoteCommand();
-    // reset(1);
+  else{
+    right = false;
+    rightCount++;
   }
+  if (!b){
+    left = true;
+    leftCount=0;
+  }
+  else{
+   left=false;
+  leftCount++;
+  }
+  if(right)
+  digitalWrite(8,1);
+  else digitalWrite(8,0);
+  if(left)
+  digitalWrite(11,1);
+  else digitalWrite(11,0);
+
+  if(rightCount>30)
+  digitalWrite(9,1);
+  if(leftCount>30)
+  digitalWrite(10,1);
+  delay(30);
+  digitalWrite(8,0);
+  digitalWrite(9,0);
+  digitalWrite(10,0);
+  digitalWrite(11,0);
+  delay(10);
 }
